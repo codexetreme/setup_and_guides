8a8ef36d84e2701252dff8ae93f551a71f268d59
Yashodhan Ghadge <yashodhanghadge@gmail.com>
2019-09-22 13:54:14 +0530

    making the setup files executable

---
+++ setup/setup_zsh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+RC_FILE=~/.zshrc
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PARENT_DIR=`realpath $CURRENT_DIR/..`
+
+if [ -f $RC_FILE ] && grep "my_zshrc.sh #v1.1" $RC_FILE
+  then echo "zrc all good."
+  else
+    set -x
+    sed -i '/^source \$ZSH\/oh-my-zsh.sh/s/^/# /' $RC_FILE
+    echo "source $PARENT_DIR/shell/zsh/my_zshrc.sh #v1.1" >> $RC_FILE
+fi
