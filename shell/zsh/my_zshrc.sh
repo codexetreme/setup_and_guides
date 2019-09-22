f48c436a18e134fc9ad99616aa13b078564c5d15
Yashodhan Ghadge <yashodhanghadge@gmail.com>
2019-09-23 01:18:19 +0530

    updating

--- shell/zsh/my_zshrc.sh
+++ shell/zsh/my_zshrc.sh
@@ -42,10 +42,10 @@ ZSH_THEME="bira"
 # DISABLE_AUTO_TITLE="true"
 
 # Uncomment the following line to enable command auto-correction.
-# ENABLE_CORRECTION="true"
+ENABLE_CORRECTION="true"
 
 # Uncomment the following line to display red dots whilst waiting for completion.
-# COMPLETION_WAITING_DOTS="true"
+COMPLETION_WAITING_DOTS="true"
 
 # Uncomment the following line if you want to disable marking untracked files
 # under VCS as dirty. This makes repository status check for large repositories
@@ -72,31 +72,6 @@ plugins=(git docker)
 
 source $ZSH/oh-my-zsh.sh
 
-# User configuration
-
-# export MANPATH="/usr/local/man:$MANPATH"
-
-# You may need to manually set your language environment
-# export LANG=en_US.UTF-8
-
-# Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='mvim'
-# fi
-
-# Compilation flags
-# export ARCHFLAGS="-arch x86_64"
-
-# Set personal aliases, overriding those provided by oh-my-zsh libs,
-# plugins, and themes. Aliases can be placed here, though oh-my-zsh
-# users are encouraged to define aliases within the ZSH_CUSTOM folder.
-# For a full list of active aliases, run `alias`.
-#
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
 alias e="exit"
 alias c="clear"
 
@@ -104,7 +79,9 @@ alias c="clear"
 export PATH=/home/${USER}/bin:$PATH
 
 
+# env vars
 
+export PROFILEDIR=/home/${USER}/my_setup
+export SOURCEDIR=/home/${USER}/src
 
-
-
+alias dotf="cd ${PROFILEDIR}"
