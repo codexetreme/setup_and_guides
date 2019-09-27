6dcfe8d59197075e7205d1d96c6148b94f49ed05
Yashodhan Ghadge <yashodhanghadge@gmail.com>
2019-09-27 20:02:56 +0530

    updating

--- shell/zsh/work_vars/gmetri/gmetri.sh
+++ shell/zsh/work_vars/gmetri/gmetri.sh
@@ -3,3 +3,21 @@ BASE_PATH=/home/${USER}/code/gmetrivr
 
 source $BASE_PATH/dec/shell/mybash_rc.sh
 source $BASE_PATH/dec/shell/mybash_vars.sh
+
+
+alias gm='cd $BASE_PATH/'
+alias tw='cd $BASE_PATH/teaxrweb'
+alias ta='cd $BASE_PATH/teaxrapi'
+alias f4='cd $BASE_PATH/fv4'
+alias dbm='cd $BASE_PATH/dbmateteaxr'
+alias wp='cd $BASE_PATH/webpages'
+alias sb='cd $BASE_PATH/storybook'
+alias tc='cd $BASE_PATH/teaxrcommon'
+alias dec='cd $BASE_PATH/dec'
+alias nmstage='cd $BASE_PATH/nm-stage'
+alias nmshreyas='cd $BASE_PATH/nm-shreyas'
+alias cstx='cd $BASE_PATH/cs-teaxr'
+alias cspg='cd $BASE_PATH/cs-pages'
+alias ghosttx='cd $BASE_PATH/ghost-teaxr'
+alias ghostgm='cd $BASE_PATH/ghost-gmetri'
+alias ghostst='cd $BASE_PATH/ghost-stsm'
