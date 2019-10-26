f10735aaac9817c7f704ac65bd35313ebec85368
Yashodhan Ghadge <yashodhanghadge@gmail.com>
2019-10-26 14:25:30 +0530

    updating

--- scripts/initProject.sh
+++ scripts/initProject.sh
@@ -2,9 +2,11 @@
 
 mkdir -p $PWD/fab/{d,sh}
 
-touch $PWD/fab/d/{base.Dockerfile, docker-compose.yaml}
+touch $PWD/fab/d/{base.Dockerfile,docker-compose.yaml}
 
-touch $PWD/{.gitignore,.dockerignore}
+touch $PWD/{.gitignore,.dockerignore,README.md}
+
 
 git init
 
+git add -A; git commit -m "inital commit";
