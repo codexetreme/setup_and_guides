82d2894b41744e4a5fc22e3fc9a816ce6e7cc335
Yashodhan Ghadge <yashodhanghadge@gmail.com>
2020-07-27 10:27:11 +0530

    updating

--- shell/zsh/work_vars/iplt/iplt.sh
+++ shell/zsh/work_vars/iplt/iplt.sh
@@ -1,3 +1,5 @@
 BASE_PATH=${HOME}/Documents/IPL
 
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/amazon-corretto-11.jdk/Contents/Home
+# export JAVA_HOME=/Library/Java/JavaVirtualMachines/amazon-corretto-11.jdk/Contents/Home
+
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/amazon-corretto-8.jdk/Contents/Home
