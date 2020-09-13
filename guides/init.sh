3955d3f4b9c8963124ec93c90842de44585563fd
Yashodhan Ghadge <yashodhanghadge@gmail.com>
2020-09-13 22:17:04 +0300

    updating

--- guides/init.sh
+++ guides/init.sh
@@ -1,12 +1,16 @@
-docker start \
+docker create \
   --name=code-server \
   -e PUID=1000 \
   -e PGID=1000 \
   -e TZ=Europe/London \
   -e PASSWORD=password `#optional` \
   -e SUDO_PASSWORD=password `#optional` \
-  -e PROXY_DOMAIN=code-server.my.domain `#optional` \
   -p 8443:8443 \
   -v $HOME/vscode_data:/config \
   --restart unless-stopped \
   linuxserver/code-server
+
+docker start code-server
+
+
+#   -e PROXY_DOMAIN=code-server.my.domain `#optional` \
