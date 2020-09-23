b1c2cc847ae4c9c6820a76e8b5c71161cd7cab01
Yashodhan Ghadge <yashodhanghadge@gmail.com>
2020-09-23 10:20:09 +0300

    updating

--- shell/zsh/work_vars/iplt/iplt.sh
+++ shell/zsh/work_vars/iplt/iplt.sh
@@ -4,4 +4,4 @@ BASE_PATH=${HOME}/Documents/IPL
 
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/amazon-corretto-8.jdk/Contents/Home
 
-export PATH="$PATH:/Users/codexetreme/Documents/istio/istio-1.7.2/bin"
+export PATH="$PATH:$HOME/Documents/istio/istio-1.7.2/bin"
