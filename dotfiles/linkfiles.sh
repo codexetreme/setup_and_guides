f0dac9274c83607b903bacb0fa3e883821e7265b
Yashodhan Ghadge <yashodhanghadge@gmail.com>
2021-03-12 04:16:11 +0530

    updating

--- dotfiles/linkfiles.sh
+++ dotfiles/linkfiles.sh
@@ -1,12 +1,17 @@
 #!/bin/zsh
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+# arg1 = path of dotfile
+# arg2 = target path
+# arg3 = use sudo 0: no, 1: yes
 function linkfile() {
     local SOURCE=$1
     local DEST=$2
     local SUDO=$3
     DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-    echo $DIR/$SOURCE
+    echo "Now applying: $DIR/$SOURCE"
     if (( $SUDO ))
     then
         [ -f $DEST -o -L $DEST ] && sudo rm $DEST
@@ -20,9 +25,19 @@ function linkfile() {
 }
 
 linkfile dotfiles/git/gitconfig             ~/.gitconfig                            0
-linkfile dotfiles/i3/i3Config               ~/.config/i3/config                     0
-linkfile dotfiles/imwheel/imwheelrc.sh      ~/.imwheelrc                            0
 linkfile dotfiles/tmux/tmux.conf            ~/.tmux.conf                            0
-linkfile dotfiles/ssh/config                ~/.ssh/config                           0
 
-sudo cp $DIR/dotfiles/i3/touchpad /etc/X11/xorg.conf.d/90-touchpad.conf
+
+case `uname` in
+  Darwin)
+    # commands for OS X go here
+    linkfile dotfiles/ssh/config_mac                ~/.ssh/config                           0
+  ;;
+  Linux)
+    # commands for Linux go here
+    linkfile dotfiles/ssh/config_linux                ~/.ssh/config                           0
+  ;;
+  FreeBSD)
+    # commands for FreeBSD go here
+  ;;
+esac
